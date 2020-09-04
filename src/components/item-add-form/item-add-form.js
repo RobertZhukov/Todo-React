4a68ce62b3a475506f26582c076f044c5b59fd61
Robert <robertzhukov97@mail.ru>
2020-09-04 19:16:40 +0300

    Work with adding a task

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,19 +3,41 @@ import React from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends React.Component {
-	render () {
+  state = {
+    label: "",
+  };
 
-		const { onItemAdded } = this.props;
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value,
+    });
+  };
 
-		return (
-			<div className="item-add-form">
-				<button 
-					className="btn btn-outline-secondary"
-					onClick={ () => onItemAdded("Hello!")}
-				> 
-					Add item
-				</button>
-			</div>	
-		)
-	}
+  onSubmit = (e) => {
+	e.preventDefault();
+	this.props.onItemAdded(this.state.label);
+	this.setState({
+		label: ''
+	  });
+  };
+
+  render() {
+    return (
+      <form className="item-add-form d-flex" 
+	  		onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done?"
+          onChange={this.onLabelChange}
+		  value={this.state.label}
+        />
+        <button
+          className="btn btn-outline-secondary"
+        >
+          Add item
+        </button>
+      </form>
+    );
+  }
 }
